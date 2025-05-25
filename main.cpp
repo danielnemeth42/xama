fdb0d416a4384330c280a3b74ed14dd9a8a766f1
Álvaro Tavares de Oliveira <ato.cdf@gmail.com>
2025-05-25 19:56:14 -0300

    Update main.cpp
    Contribuição

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string>
+#include <format>
 
 using namespace std;
 
 struct player {
 	int hp;
-	int coins; 
-	int atk;	
-	};
+	int coins;
+	int atk;
+
+	string to_string(){
+		return "HP: " + std::to_string(hp) + " Coins: " + std::to_string(coins) + " Atk: " + std::to_string(atk);
+	}
+};
 
 int main (){
 	player p1;
 	p1.hp = 100;
 	p1.coins = 0;
 	p1.atk = 3;
-	cout << "o progama esta funcionando corretamente!\n";
+	cout << p1.to_string() << " o progama esta funcionando corretamente!\n";
 	return 0;
 }
